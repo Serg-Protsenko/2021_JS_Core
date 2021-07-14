e1ef2d36dbacb5802b23e7c8dbd1159c834e962b
Serg <shaytanbat@gmail.com>
2021-07-14 15:12:44 +0300

    Add own myFilter function

--- homework_03/index.js
+++ homework_03/index.js
@@ -26,16 +26,33 @@ function isPalindrome(str) {
 
 }
 
-// alert(`"Ann,a" palindrome is ${isPalindrome('Ann,a')}`)
-// alert(`"Ann" palindrome is ${isPalindrome('Ann')}`)
-// alert(`" eye" palindrome is ${isPalindrome(' eye')}`)
-// alert(`"Yes" palindrome is ${isPalindrome('Yes')}`)
+// console.log(`"Ann,a" palindrome is ${isPalindrome('Ann,a')}`)
+// console.log(`"Ann" palindrome is ${isPalindrome('Ann')}`)
+// console.log(`" eye" palindrome is ${isPalindrome(' eye')}`)
+// console.log(`"Yes" palindrome is ${isPalindrome('Yes')}`)
 
 function findVowels(str) {
     return (str.match(/[aeiou]/ig) || []).length;
 }
 
-// alert(`"yes no " has ${findVowels('yes no ')} vowels.`);
-// alert(`"Hello, World!" has ${findVowels('Hello, World!')} vowels.`);
-// alert(`"Why?" has ${findVowels('Why?')} vowels.`);
+// console.log(`"yes no " has ${findVowels('yes no ')} vowels.`);
+// console.log(`"Hello, World!" has ${findVowels('Hello, World!')} vowels.`);
+// console.log(`"Why?" has ${findVowels('Why?')} vowels.`);
 
+function myFilter(arr, callback) {
+    const resultArr = [];
+    for (let i = 0; i < arr.length; i++){
+        const result = callback(arr[i], i, arr);
+        if (result) {
+            resultArr.push(arr[i]);
+        }
+    } return resultArr;
+}
+
+// const arrTest = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const isOdd = num => num % 2 !== 0;
+// console.log(`For array ${arrTest} odd numbers are ${myFilter(arrTest, isOdd)}.`);
+
+// const isEven = num => num % 2 === 0;
+// const evenArr = myFilter(arrTest, isEven);
+// console.log(`For array ${arrTest} odd numbers are ${evenArr}.`);
