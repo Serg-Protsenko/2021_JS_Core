dfb6770ba191c5caf2fa323f6bfd97a76e8e1d1b
Serg <shaytanbat@gmail.com>
2021-07-18 15:23:30 +0300

    Create function createDebounceFunction

--- homework_04/index.js
+++ homework_04/index.js
@@ -5,3 +5,17 @@ function stringToObject (str){
 
 // console.log(stringToObject('a.b.c.d'));
 
+
+function createDebounceFunction(callback, ms) {
+    let alreadyRun = false;
+    return () => {
+        if (alreadyRun) clearTimeout(alreadyRun);
+        alreadyRun = setTimeout(callback, ms);
+    }
+}
+
+// const log100 = () => console.log(100);
+// const debounceLog100 = createDebounceFunction(log100, 1000);
+// debounceLog100()
+// setTimeout(debounceLog100, 200);
+
