df6bbdec992bfc009ef1620ae5354a8bb8df5b69
Serg <shaytanbat@gmail.com>
2021-07-21 18:16:29 +0300

    Task 02 PromiseAll polyfill

--- homework_05/index.js
+++ homework_05/index.js
@@ -8,4 +8,30 @@ button.addEventListener('click', () => {
     console.log('Listener 2');
 });
 
-button.click();
+// button.click();
+
+
+async function promiseAll(urls) {
+    
+    const fakeRequest = (url) => {
+        return new Promise((res, rej) => {            
+            const delayTime = Math.floor(Math.random() * 1000) + 1;
+
+            setTimeout(() => res(url), delayTime);
+        });
+    }
+    const result = [];
+    for (let i = 0; i < urls.length; i++) {
+        const response = await fakeRequest(urls[i]);
+        result.push(response);
+
+        console.log(`Got response for url: ${urls[i]}`)
+    }
+
+    return Promise.resolve(result);
+}
+
+// const urls = ['1','2','3','4','5'];
+// promiseAll(urls).then((res) => {
+//     console.log(`All urls was processed. Result: ${JSON.stringify(res)}`)
+// });
