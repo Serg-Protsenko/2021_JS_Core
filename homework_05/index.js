8908ac62d2bb262be244528413054ba7c924d306
Serg <shaytanbat@gmail.com>
2021-07-23 12:25:08 +0300

    Add forEach to promiseAll function

--- homework_05/index.js
+++ homework_05/index.js
@@ -17,6 +17,7 @@ button.addEventListener("click", () => {
 function fakeRequest(url) {
   return new Promise((res, rej) => {
     const delayTime = Math.floor(Math.random() * 1000) + 1;
+
     setTimeout(() => res(url), delayTime);
   });
 }
@@ -24,15 +25,21 @@ function fakeRequest(url) {
 async function promiseAll(urls) {
   const result = [];
 
-  for (let i = 0; i < urls.length; i++) {
-    const response = await fakeRequest(urls[i]);
+  urls.forEach(async (url) => {
+    const response = await fakeRequest(url);
     result.push(response);
-    console.log(`Got response for url: ${urls[i]}`);
-  }
+    console.log(`Got response for url: ${url}`);
+  });
+
+  //   for (let i = 0; i < urls.length; i++) {
+  //     const response = await fakeRequest(urls[i]);
+  //     result.push(response);
+  //     console.log(`Got response for url: ${urls[i]}`);
+  //   }
   return Promise.resolve(result);
 }
 
-// const urls = ["1", "2", "3", "4", "5"];
-// promiseAll(urls).then((res) => {
-//   console.log(`All urls was processed. Result: ${JSON.stringify(res)}`);
-// });
+const urls = ["1", "2", "3", "4", "5"];
+promiseAll(urls).then((res) => {
+  console.log(`All urls was processed. Result: ${JSON.stringify(res)}`);
+});
